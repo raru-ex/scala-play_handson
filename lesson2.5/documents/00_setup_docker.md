9cadbb5a2678499529f0bc7cd3616f72c1b1c0a8
raru-ex <raru.okiraku@gmail.com>
2020-04-20 21:33:45 +0900

    add angular-elements

---
+++ lesson2.5/documents/00_setup_docker.md
@@ -0,0 +1,19 @@
+summary: Play Framework Handson Lesson2.5
+id: play-handson-lesson2.5
+enviroments: web
+status: Draft
+feedback link: https://github.com/Christina-Inching-Triceps/scala-play_handson/issues
+tags: scala,Play Framework,Angular
+authors: chiristina.inching.triceps
+
+# Play Framewworkハンズオン 2.5章
+
+今回は個人的な興味でPlayFrameworkの環境にAngular-Elementsを導入してきます。  
+Vue, Reactがお好みの方やAngular-Elementsが不要な方はスキップしてしまってください。  
+
+## Docker環境のセットアップ
+
+
+### Lesson1のプロジェクトのコピー
+
+
---
+++ lesson2.5/example/play-handson/app/slick/samples/SlickMappedMySQLDateTime.scala
@@ -0,0 +1,25 @@
+package slick.samples
+
+import java.time.format.DateTimeFormatter
+import java.time.LocalDateTime
+
+/* MySQLDateTimeを直接マッピング対象にできないので、mappingに利用するクラスを作成する */
+case class MySQLDateTime(v: String) {
+  def toLocalDateTime: LocalDateTime = LocalDateTime.parse(v, MySQLDateTime.format)
+}
+
+object MySQLDateTime {
+  val format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+  def apply(time: LocalDateTime): MySQLDateTime = MySQLDateTime(time.format(format))
+}
+
+
+trait LocalDateTimeColumMapper {
+  val profile: slick.jdbc.JdbcProfile
+  import profile.api._
+
+  implicit lazy val localDateTimeMapper = MappedColumnType.base[MySQLDateTime, String] (
+    { ldt => ldt.v },
+    { str => MySQLDateTime(str)}
+  )
+}
